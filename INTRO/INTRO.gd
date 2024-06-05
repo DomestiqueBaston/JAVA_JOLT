b94767184a3c8841ff1bc1a2b8e9dc889eb25340
DomestiqueBaston <ferdi2x@gmail.com>
2024-06-05 11:18:54 +0200

    Creating scenes

--- INTRO/INTRO.gd
+++ INTRO/INTRO.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_animation_screens_animation_finished(_anim_name):
-	get_tree().change_scene_to_file("res://DEMO/JAVA_JOLT.tscn")
+	get_tree().change_scene_to_file("res://JAVA_JOLT.tscn")
