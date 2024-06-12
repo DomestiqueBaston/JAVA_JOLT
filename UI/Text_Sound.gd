dac23907265f8eae983a2c010a2a6a1a2dd02c06
DomestiqueBaston <ferdi2x@gmail.com>
2024-06-12 21:36:17 +0200

    Still preparing background. + important and useless fixes...

--- UI/Text_Sound.gd
+++ UI/Text_Sound.gd
@@ -7,7 +7,8 @@ var actual_value = 0
 func _process(_delta):
 	# There is an unwanted delay with the typing sound...
 	if $".".visible_characters > actual_value && $"..".visible == true:
-		$"../../../../Typing".play()
+		$"../../Typing".play()
+		
 		actual_value += 1
 	else:
 		actual_value = 0
