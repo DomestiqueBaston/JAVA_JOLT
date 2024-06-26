841237a4095012b73df9ee98f87855e8b896110b
Geoff Levner <glevner@gmail.com>
2024-06-26 17:22:43 +0200

    Dialogue tweaks. Typing is faster and controlled by the "characters per second" property. The "..." control appears only as needed and is not red. Clicking outside the "..." control skips to the end of the current phrase.

--- UI/UI.gd
+++ UI/UI.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 enum { ROWENA, DOCTOR }
 
+@export var characters_per_second := 15
 @export var rowena_text_color := Color(0xed/255.0, 0x6f/255.0, 0x68/255.0)
 @export var doctor_text_color := Color(0x5b/255.0, 0x90/255.0, 0xc2/255.0)
 
@@ -31,33 +32,50 @@ func _set_dialogue_color(speaker: int):
 
 func clear_dialogue():
 	$Dialogue_AnimationPlayer.play("Close_Dialogue")
+	$Boxes/Dialogue_Box/Next.visible = false
 
 func set_dialogue_text(text: String, speaker: int):
 	_set_dialogue_color(speaker)
 	$Boxes/Dialogue_Box/Dialogue.text = text
 	$Dialogue_AnimationPlayer.play("Open_Dialogue")
+	$Boxes/Dialogue_Box/Next.visible = true
 
 func type_dialogue_text(text: String, speaker: int):
 	_set_dialogue_color(speaker)
 	$Boxes/Dialogue_Box/Dialogue.text = text
 	$Boxes/Dialogue_Box/Dialogue.visible_characters = 0
-	$Boxes/Dialogue_Box/Typing_Timer.start()
+	$Boxes/Dialogue_Box/Next.visible = false
+	$Boxes/Dialogue_Box/Typing_Timer.start(1.0 / characters_per_second)
 	$Dialogue_AnimationPlayer.play("Open_Dialogue")
 
 func _type_one_character():
 	var n = $Boxes/Dialogue_Box/Dialogue.text.length()
 	var i = $Boxes/Dialogue_Box/Dialogue.visible_characters
-	while i < n:
-		i += 1
-		if $Boxes/Dialogue_Box/Dialogue.text[i-1] != ' ':
-			$Typing.play()
-			break
-	$Boxes/Dialogue_Box/Dialogue.visible_characters = i
-	if i == n:
+	if i >= 0:
+		while i < n:
+			i += 1
+			if $Boxes/Dialogue_Box/Dialogue.text[i-1] != ' ':
+				$Typing.play()
+				break
+		$Boxes/Dialogue_Box/Dialogue.visible_characters = i
+	if i < 0 or i == n:
 		$Boxes/Dialogue_Box/Typing_Timer.stop()
 		emit_signal("_typing_finished")
+		$Boxes/Dialogue_Box/Next.visible = true
 
+#
+# If the user clicks on the three points control, emit a _next_click signal.
+#
 func _on_three_points_gui_input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			emit_signal("_next_click")
+
+#
+# If the user clicks on the dialogue box outside of the three points control,
+# skip to the end of the current text.
+#
+func _on_dialogue_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			$Boxes/Dialogue_Box/Dialogue.visible_characters = -1
