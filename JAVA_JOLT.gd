02b7b4c61a09a40a557b5d15cd3d38b9be47386b
Geoff Levner <glevner@gmail.com>
2024-07-11 17:11:39 +0200

    Click on right window quits the game.

--- JAVA_JOLT.gd
+++ JAVA_JOLT.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
+var current_object = -1
+
 func _on_ui_click_on_background(pos):
-	$ROWENA.walk_to(pos.x)
+	if current_object == $BACKGROUND.WINDOW_RIGHT:
+		get_tree().quit()
+	else:
+		$ROWENA.walk_to(pos.x)
 
 func _on_background_mouse_entered_object(which):
 	print($BACKGROUND.get_collider(which).name)
+	current_object = which
+	if which == $BACKGROUND.WINDOW_RIGHT:
+		$UI.set_mouse_cursor("Quit")
+	else:
+		$UI.set_mouse_cursor("Cross_Passive")
 
 func _on_background_mouse_exited_object(_which):
-	pass
+	current_object = -1
+	$UI.set_mouse_cursor("Cross_Passive")
--- UI/New_Mouse.gd
+++ UI/New_Mouse.gd
@@ -1,10 +1,19 @@
 extends Control
 
+var current_cursor: TextureRect
+
 func _ready():
 	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	show()
-	$Cross_Passive.show()
+	set_cursor("Cross_Passive")
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		global_position = event.position
+
+func set_cursor(cursor_name: String):
+	var cursor = get_node(cursor_name)
+	if cursor != null and cursor != current_cursor:
+		if current_cursor:
+			current_cursor.hide()
+		current_cursor = cursor
+		current_cursor.show()
