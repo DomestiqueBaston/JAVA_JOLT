a181388cad6b92462b733735fa5a0ec0c5e95c57
DomestiqueBaston <ferdi2x@gmail.com>
2024-08-31 18:11:15 +0200

    Enlarged ? collider (+ preload tweak ^_^')

--- INTRO_OUTRO/INTRO.gd
+++ INTRO_OUTRO/INTRO.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
 
+var next_scene = load("res://JAVA_JOLT.tscn")
+
 func _on_animation_screens_animation_finished(_anim_name):
-	get_tree().change_scene_to_file("res://JAVA_JOLT.tscn")
+	get_tree().change_scene_to_packed(next_scene)
