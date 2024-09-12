22ea56a2e1d2046c8e481278414988ea6111147c
Geoff Levner <glevner@gmail.com>
2024-09-12 11:11:47 +0200

    Cleaned up TOP_LEVEL script.

--- TOP_LEVEL.gd
+++ TOP_LEVEL.gd
@@ -1,13 +1,8 @@
 extends Node
 
-var the_game = preload("res://JAVA_JOLT.tscn")
-
 func _on_intro_radio_on():
 	$RadioPlayer.play()
 
 func _on_intro_intro_done():
 	$INTRO.queue_free()
-	#var game_instance = the_game.instantiate()
-	#game_instance.skip_dialogues = false
-	var game_instance = $JAVA_JOLT.create_instance()
-	add_child(game_instance)
+	add_child($JAVA_JOLT.create_instance())
