e20619a8d31aeec37b32aa4a464d1b9fe740c989
Geoff Levner <glevner@gmail.com>
2024-09-12 15:47:52 +0200

    TOP_LEVEL: do as much work as possible on startup.

--- TOP_LEVEL.gd
+++ TOP_LEVEL.gd
@@ -1,12 +1,17 @@
 extends Node
 
-var game_scene = preload("res://JAVA_JOLT.tscn")
+var intro = preload("res://INTRO_OUTRO/INTRO.tscn").instantiate()
+var game = preload("res://JAVA_JOLT.tscn").instantiate()
+
+func _ready():
+	add_child(intro)
+	intro.radio_on.connect(_on_intro_radio_on)
+	intro.intro_done.connect(_on_intro_intro_done)
+	game.skip_dialogues = false
 
 func _on_intro_radio_on():
 	$RadioPlayer.play()
 
 func _on_intro_intro_done():
-	$INTRO.queue_free()
-	var game = game_scene.instantiate()
-	game.skip_dialogues = false
+	intro.queue_free()
 	add_child(game)
