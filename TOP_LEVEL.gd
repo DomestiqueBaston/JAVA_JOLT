940fc064b77766b4a3325bc3fad4b751d748d0a4
Geoff Levner <glevner@gmail.com>
2024-09-12 15:24:43 +0200

    Preload JAVA_JOLT scene (faster?).

--- TOP_LEVEL.gd
+++ TOP_LEVEL.gd
@@ -1,8 +1,12 @@
 extends Node
 
+var game_scene = preload("res://JAVA_JOLT.tscn")
+
 func _on_intro_radio_on():
 	$RadioPlayer.play()
 
 func _on_intro_intro_done():
 	$INTRO.queue_free()
-	$JAVA_JOLT.create_instance()
+	var game = game_scene.instantiate()
+	game.skip_dialogues = false
+	add_child(game)
